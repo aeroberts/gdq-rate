c33fd78ee67ac528dc49a4d7b8e2b06f42d25e0b
Alex Roberts <aerobs@umich.edu>
2020-07-26 18:46:46 -0700

    Added formik

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,31 +2,58 @@ import React from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Formik } from 'formik';
 
 export default function Login() {
-    return (
-        <Card>
-            <Card.Body>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-            </Form.Text>
-                    </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                </Button>
-                </Form>
+
+    return (
+        <Card id="login-container">
+            <Card.Body>
+                <Formik
+                    initialValues={{ username: '', password: '', checkbox: false }}
+                    onSubmit={async values => {
+                        await new Promise(r => setTimeout(r, 2000));
+                    }}>
+                    {({
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        values,
+                        touched,
+                        isValid,
+                        errors,
+                        isSubmitting
+                    }) => (
+                            <Form onSubmit={handleSubmit} className={isSubmitting ? "is-submitting" : ""}>
+                                <Form.Group controlId="username">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control 
+                                        type="text" 
+                                        placeholder="Enter username" 
+                                        name="username"
+                                        value={values.username}
+                                        onChange={handleChange} />
+                                </Form.Group>
+
+                                <Form.Group controlId="password-2">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control 
+                                        type="password" 
+                                        placeholder="Password"
+                                        name="password"
+                                        value={values.password}
+                                        onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group controlId="nice">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit" disabled={isSubmitting}>
+                                    Submit
+                                </Button>
+                            </Form>
+                        )}
+                </Formik>
             </Card.Body>
         </Card>
     );
