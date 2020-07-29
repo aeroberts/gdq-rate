50ad62fa465896c60a643ea41408c64f6418ea38
Shane Schulte <shane.schulte@samsara.com>
2020-07-29 00:32:18 -0700

    oops

--- src/routes/run.tsx
+++ src/routes/run.tsx
@@ -4,16 +4,16 @@ import { useGetSpecificRunSubscription } from "../generated/graphql";
 import { AuthContext } from "../contexts/gdq-rate-auth";
 
 export default function Run() {
-  const { runId } = useParams();
-  const { userData } = React.useContext(AuthContext);
-  const { loading, error, data } = useGetSpecificRunSubscription({
-    variables: {
-      loggedIn: !!userData,
-      userId: userData && userData.id,
-      runId,
-    },
-  });
-  if (!data) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return <pre>{JSON.stringify(data!.runs[0], null, 2)}</pre>;
+    const { runId } = useParams();
+    const { userData } = React.useContext(AuthContext);
+    const { loading, error, data } = useGetSpecificRunSubscription({
+        variables: {
+            loggedIn: !!userData,
+            userId: userData && userData.id,
+            runId
+        }
+    });
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    return <pre>{JSON.stringify(data!.runs[0], null, 2)}</pre>;
 }
