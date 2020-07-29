71724aa01abe36704dcf88dc61586256c9fd762b
Shane Schulte <shane.schulte@samsara.com>
2020-07-29 00:32:18 -0700

    oops

--- src/routes/runs.tsx
+++ src/routes/runs.tsx
@@ -4,16 +4,16 @@ import { AuthContext } from "../contexts/gdq-rate-auth";
 import { useGetAllRunsSubscription } from "../generated/graphql";
 
 export default function Runs() {
-  const { userData } = React.useContext(AuthContext);
-  const { loading, error, data } = useGetAllRunsSubscription({
-    variables: {
-      loggedIn: !!userData,
-      userId: userData && userData.id,
-    },
-  });
+    const { userData } = React.useContext(AuthContext);
+    const { loading, error, data } = useGetAllRunsSubscription({
+        variables: {
+            loggedIn: !!userData,
+            userId: userData && userData.id
+        }
+    });
 
-  if (!data) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  return <RunsTable runs={data!.runs} loggedIn={!!userData} />;
+    return <RunsTable runs={data!.runs} loggedIn={!!userData} />;
 }
