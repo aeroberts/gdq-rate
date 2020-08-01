ac8a2c8248c1c53ca324682fd67b101e5869dacf
Shane Schulte <shane.schulte@samsara.com>
2020-08-01 01:15:37 -0700

    fix logout

--- src/routes/logout.tsx
+++ src/routes/logout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../contexts/gdq-rate-auth";
+import { useApolloClient } from "@apollo/react-hooks";
+import { UserInfoDocument } from "../generated/graphql";
 
 export default function Logout() {
   const [redirect, setRedirect] = React.useState(false);
   const { refetch } = React.useContext(AuthContext);
 
+  const client = useApolloClient();
   React.useEffect(() => {
     (async function () {
       await window.fetch("/rest/auth/logout", { method: "POST" });
@@ -13,11 +16,15 @@ export default function Logout() {
       try {
         await refetch();
       } catch (e) {
-        console.error(e);
+        // Clear out cache on error
+        client.writeQuery({
+          query: UserInfoDocument,
+          data: {},
+        });
       }
       setRedirect(true);
     })();
-  }, [refetch]);
+  }, [refetch, client]);
 
   if (redirect) {
     return <Redirect to={"/"} />;
