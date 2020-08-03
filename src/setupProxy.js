34917d5952c926de7c84be552a3bae609745b1a4
Shane Schulte <shane.schulte@samsara.com>
2020-08-02 18:29:33 -0700

    fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     "/hasura/v1/graphql",
-    createProxyMiddleware({
+    createProxyMiddleware("/hasura/v1/graphql", {
       target: "https://sgdq.shaneschulte.com",
       changeOrigin: true,
       ws: true,
